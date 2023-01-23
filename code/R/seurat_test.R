398dc9c064ebc55e780f746d4dccdad0effdc608
Jeremy Yang <jjyang@bosque.health.unm.edu>
2023-01-23 13:55:10 -0700

    Alright.

--- code/R/seurat_test.R
+++ code/R/seurat_test.R
@@ -1,9 +1,11 @@
-library(dplyr)
+#!/usr/bin/env Rscript
+###
+library(dplyr, quietly=T, warn.conflicts=F)
 library(Seurat)
 library(patchwork)
 
 # SeuratObject
-data_01 <- readRDS("local_01.rds")
+data_01 <- readRDS(paste0(Sys.getenv("HOME"), "/../data/CZBiohub/TabulaSapiens/data/collection_Alzheimer_Leng_2021/", "local_01.rds"))
 
 data_01
 
